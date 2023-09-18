5ca49693750e5955bd281975d446baa3d867bb70
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-18 15:24:30 +0900

    Drill 2 제출 (2018184042) (Fix)
    Drill#2_2.py 만들어서 캐릭터가 원을 그릴 수 있도록 하였습니다.

--- Drill#2_2.py
+++ Drill#2_2.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import math
+
 math.sin(270 / 360 * 2 * math.pi)
          
 open_canvas()
@@ -7,7 +8,11 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+val = 270
 while(True):
+    
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(, y)
+    character.draw_now(400 + 400*(math.cos(val / 360 * 2 * math.pi)), 360 +270*(math.sin(val / 360 * 2 * math.pi)) )
+    val = val + 1
+    delay(0.01)
